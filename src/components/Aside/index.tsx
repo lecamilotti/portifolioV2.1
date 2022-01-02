27a42e3bdf5376a9258dafc3b3978730fdceb214
Leandro Camilotti <lecamilotti@gmail.com>
2022-01-02 13:14:48 +0100

    aside img

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,11 +1,18 @@
 import { Box, Flex } from "@chakra-ui/react";
+import Image from "next/image";
 
 
 export default function Aside(){
   return (
     <Flex maxW={1280} m="auto"  width={["100%", "100%", "100%", "100%"]}>
       <Box justifyContent="center" m="auto" p="20">
+      <Image
+      src="/../public/images/programer.png"
+      alt="programer"
+      width="500"
+      height="400"
       
+      />
         
        </Box>
     </Flex>
