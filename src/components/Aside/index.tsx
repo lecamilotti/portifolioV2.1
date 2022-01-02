1207d9ac1204d87bfe711d13ce842553da70fe2d
Leandro Camilotti <lecamilotti@gmail.com>
2022-01-02 13:16:06 +0100

    margin

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function Aside(){
   return (
     <Flex maxW={1280} m="auto"  width={["100%", "100%", "100%", "100%"]}>
-      <Box justifyContent="center" m="auto" p="20">
+      <Box justifyContent="center" ml="auto" p="20">
       <Image
       src="/../public/images/programer.png"
       alt="programer"
