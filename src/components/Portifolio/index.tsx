2140d6282cb9bd1a5c6af95f9e6e28cf74604ecd
Leandro Camilotti <lecamilotti@gmail.com>
2022-01-05 11:07:44 +0100

    data

--- src/components/Portifolio/index.tsx
+++ src/components/Portifolio/index.tsx
@@ -1,33 +1,52 @@
-import { Box, Button, HStack, Image, SimpleGrid} from "@chakra-ui/react";
-import {FaFacebook, FaGithub, FaGlobe, FaTwitter} from "react-icons/fa";
-import ProjectsData from "../data";
+import { Box, Button, HStack, Image, SimpleGrid, Text } from "@chakra-ui/react";
+import { FaGithub, FaGlobe } from "react-icons/fa";
+import projects from "../projects";
 
-export default function Portifolio() {
+interface projectProps {
+  name: string;
+  description: string;
+  image: string;
+  alt: string;
+  link: string;
+  gitHub: string;
+}
+
+const myProjects = projects.map((project: projectProps) => {
+  
   return (
     <SimpleGrid
-    w="1280px"
-    p={{ sm: '10', md: '10', lg: '10', xl: '10' }}
-    bg='gray.800'
-    columns={{ sm: 1, md: 2, lg: 3, xl: 4}}
-    spacing='4'
-    textAlign='center'
-    rounded='lg'
-    color='gray.500'
-  >
-    <Box boxShadow='lg' maxW={400} maxH={400} rounded='lg' bg='white'>
-       {ProjectsData.name}
-    <HStack>
-      <Button colorScheme='gray' leftIcon={<FaGithub />}>
-        Github
-      </Button>
-     <Button colorScheme='gray' leftIcon={<FaGlobe />}>
-      Live Demo
-  </Button>
-</HStack>
-</Box>
-    
-    
-  </SimpleGrid>
-  
+      w="1280px"
+      p={{ sm: "10", md: "10", lg: "10", xl: "10" }}
+      bg="gray.800"
+      columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+      spacing="4"
+      textAlign="center"
+      rounded="lg"
+      color="gray.500"
+    >
+      <Box
+        key={myProjects.name}
+        boxShadow="lg"
+        maxW={400}
+        maxH={400}
+        rounded="lg"
+        bg="white"
+      >
+        <HStack>
+          <Image src={myProjects.image} size="150px" alt="" />
+          <Box>
+            <Text>{myProjects.name}</Text>
+            <Text>{myProjects.description}</Text>
+            <Button colorScheme="gray" leftIcon={<FaGithub />}>
+              Github<Text>{myProjects.gitHub}</Text>
+            </Button>
+            <Button colorScheme="gray" leftIcon={<FaGlobe />}>
+              Live Demo <Text>{myProjects.link}</Text>
+            </Button>
+          </Box>
+        </HStack>
+      </Box>
+    </SimpleGrid>
   );
-}
+})
+export default myProjects;
--- src/components/projects.ts
+++ src/components/projects.ts
@@ -1,10 +1,11 @@
 
-export default function ProjectsData() {
-const ProjectsData = [
+
+const projects = [
   {
     name: "DogGromming",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     image: "https://via.placeholder.com/150",
+    alt: "DogGromming",
     link: "https://www.google.com",
     gitHub: "https://www.github.com",
   },
@@ -12,6 +13,7 @@ const ProjectsData = [
     name: "DotMoney",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     image: "https://via.placeholder.com/150",
+    alt: "Image of DotMoney",
     link: "https://www.google.com",
     gitHub: "https://www.github.com",
   },
@@ -20,7 +22,9 @@ const ProjectsData = [
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     image: "https://via.placeholder.com/150",
     link: "https://www.google.com",
+    alt: "Image of E-commerce",
     gitHub: "https://www.github.com",
   },
   ]
-}
+;
+export default projects;
