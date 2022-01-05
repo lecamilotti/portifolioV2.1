50badae50cdbf0f1c57d227a8604e19d55e72e21
Leandro Camilotti <lecamilotti@gmail.com>
2022-01-05 12:16:23 +0100

    fixing layout

--- src/components/Portifolio/index.tsx
+++ src/components/Portifolio/index.tsx
@@ -1,4 +1,12 @@
-import { Box, Button, Flex, HStack, Image, SimpleGrid, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  HStack,
+  Image,
+  SimpleGrid,
+  Text,
+} from "@chakra-ui/react";
 import { FaGithub, FaGlobe } from "react-icons/fa";
 import projects from "../projects";
 
@@ -12,50 +20,64 @@ interface projectProps {
 }
 
 const myProjects = () => {
-  return projects.map((project: projectProps) => { 
+  return projects.map((project: projectProps) => {
     return (
-      <SimpleGrid
-        w="1280px"
-        p={{ sm: "10", md: "10", lg: "10", xl: "10" }}
-        bg="gray.800"
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        spacing="4"
-        textAlign="center"
-        rounded="lg"
-        color="gray.500"
-        key={project.name}
-      >
-        <Box
-          key={project.name}
-          boxShadow="lg"
-          maxW={400}
-          maxH={400}
-          rounded="lg"
-          bg="white"
+      <>
+        <Text
+          as="title"
+          fontSize="2xl"
+          fontWeight="bold"
+          mb={4}
+          color="gray.800"
+          justifyContent="center"
         >
-          <HStack>
-            <Image src={project.image} size="150px" alt="" />
-            <Box>
+          Projects
+        </Text>
+        <SimpleGrid
+          w="1280px"
+          p={{ sm: "10", md: "10", lg: "10", xl: "10" }}
+          bg="gray.800"
+          columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+          spacing="4"
+          textAlign="center"
+          rounded="lg"
+          color="gray.500"
+          key={project.name}
+        >
+          <Box
+            key={project.name}
+            boxShadow="lg"
+            maxW={400}
+            maxH={400}
+            rounded="lg"
+            bg="white"
+          >
+            <Image src={project.image} size="500px" alt={project.alt} />
+            <HStack>
               <Text>{project.name}</Text>
               <Text>{project.description}</Text>
-              <Button colorScheme="gray" leftIcon={<FaGithub />}>
-                Github<Text>{project.gitHub}</Text>
+              <Button
+                colorScheme="gray"
+                onClick={project.gitHub}
+                leftIcon={<FaGithub />}
+              >
+                Github
               </Button>
-              <Button colorScheme="gray" leftIcon={<FaGlobe />}>
-                Live Demo <Text>{project.link}</Text>
+              <Button
+                onClick={project.link}
+                colorScheme="gray"
+                leftIcon={<FaGlobe />}
+              >
+                Live Demo
               </Button>
-            </Box>
-          </HStack>
-        </Box>
-      </SimpleGrid>
+            </HStack>
+          </Box>
+        </SimpleGrid>
+      </>
     );
-  })
-}
+  });
+};
 
 export default function Portifolio() {
-  return (
-    <Flex justify="center">
-      {myProjects()}
-    </Flex>
-  )
+  return <Flex justify="center">{myProjects()}</Flex>;
 }
--- src/components/projects.ts
+++ src/components/projects.ts
@@ -9,22 +9,22 @@ const projects = [
     link: "https://www.google.com",
     gitHub: "https://www.github.com",
   },
-  {
-    name: "DotMoney",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    image: "https://via.placeholder.com/150",
-    alt: "Image of DotMoney",
-    link: "https://www.google.com",
-    gitHub: "https://www.github.com",
-  },
-  {
-    name: "E-commerce",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    image: "https://via.placeholder.com/150",
-    link: "https://www.google.com",
-    alt: "Image of E-commerce",
-    gitHub: "https://www.github.com",
-  },
+  // {
+  //   name: "DotMoney",
+  //   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  //   image: "https://via.placeholder.com/150",
+  //   alt: "Image of DotMoney",
+  //   link: "https://www.google.com",
+  //   gitHub: "https://www.github.com",
+  // },
+  // {
+  //   name: "E-commerce",
+  //   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  //   image: "https://via.placeholder.com/150",
+  //   link: "https://www.google.com",
+  //   alt: "Image of E-commerce",
+  //   gitHub: "https://www.github.com",
+  // },
   ]
 ;
 export default projects;
