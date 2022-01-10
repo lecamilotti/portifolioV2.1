0720ee0199d2a42ee7c1657b66e205744707edd4
Leandro Camilotti <lecamilotti@gmail.com>
2022-01-10 20:18:29 +0100

    still working on grid :(

--- src/components/Portifolio/index.tsx
+++ src/components/Portifolio/index.tsx
@@ -2,6 +2,7 @@ import {
   Box,
   Button,
   Flex,
+  GridItem,
   HStack,
   Image,
   SimpleGrid,
@@ -23,106 +24,75 @@ const myProjects = () => {
   return projects.map((project: projectProps) => {
     return (
       <SimpleGrid
-      templateColumns="repeat(auto-fit(2, 3fr))"
-      gridAutoFlow={["column", "row"]}
-        key={project.name}
-        
-        
-        spacing={[10, 10, 10, 10]}
-       
-        textAlign="center"
-        justifyContent={["center", "center", "center", "center"]}
-        alignItems="center"
-        rounded="lg"
-        color="gray.600"
-        p={[10, 10, 10, 10]}
-        
+      key={project.name}
+      minChildWidth='350px'
+      spacing='40px'     
       >
-        {/* <Text
-          as="title"
-          fontSize="2xl"
-          m="auto"
-          p="auto"
-          justifyContent="center"
-          alignItems="center"
-          fontWeight="bold"
-        >
-          Projects
-        </Text> */}
-        <Box
-
-          key={project.name}
-          boxShadow="2xl"
-          w="28rem"
-          maxW="30rem"
-          rounded="2xl"
-          shadow={["lg", "lg", "lg", "lg"]}
-          bg="gray.700"
-          m="auto"
-          pt="3"
-          pb="3"
-          
-        >
-          <Text color="whiteAlpha.800" fontWeight="bold" fontSize="2xl">
-            {project.name}
-          </Text>
-          <HStack>
-            <Image
-              src={project.image}
-              justifyContent="center"
-              m="auto"
-              p="auto"
-              maxW="500px"
-              fit="scale-down"
-              width={["100%", "100%", "100%", "100%"]}
-              align="center"
-              objectFit="cover"
-              alt={project.alt}
-            />
-
-            {/* <Text>{project.description}</Text> */}
-          </HStack>
-          <Button
-            colorScheme="gray"
-            variant="outline"
+        <Box>
+          <Box
+            as="a"
             href={project.link}
             target="_blank"
-            transition="all 0.3s"
-            _hover={{ filter: "brightness(0.5)" }}
-            leftIcon={<FaGithub />}
-            m="2"
+            rel="noopener noreferrer"
           >
-            Github
-          </Button>
-          <Button
-            onClick={project.link}
-            colorScheme="gray"
-            transition="all 0.3s"
-            _hover={{ filter: "brightness(0.5)" }}
-            m="2"
-            leftIcon={<FaGlobe />}
-          >
-            Live Demo
-          </Button>
-          <Button
-            onClick={project.description}
-            colorScheme="gray"
-            transition="all 0.3s"
-            _hover={{ filter: "brightness(0.5)" }}
-            m="2"
-            leftIcon={<FaInfo />}
-          >
-            Description
-          </Button>
+            <Image
+              src={project.image}
+              alt={project.alt}
+              maxWidth="100%"
+              maxHeight="100%"
+              objectFit="cover"
+            />
+          </Box>
         </Box>
+        <GridItem>
+          <Flex justifyContent="center" alignItems="center" w="100%">
+            <HStack spacing="0.5rem">
+              <Button
+                as="a"
+                href={project.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                variant="outline"
+                color="gray.50"
+                leftIcon={<FaGlobe/>}
+              >
+                <Text fontSize="sm">Live</Text>
+              </Button>
+              <Button
+                as="a"
+                href={project.gitHub}
+                target="_blank"
+                rel="noopener noreferrer"
+                variant="outline"
+                color="gray.50"
+                leftIcon={<FaGithub />}
+              >
+                <Text fontSize="sm">GitHub</Text>
+              </Button>
+              <Button
+                as="a"
+                href={project.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                variant="outline"
+                color="gray.50"
+                leftIcon={<FaInfo />}
+              >
+                <Text fontSize="sm">Info</Text>
+              </Button>
+            </HStack>
+          </Flex>
+          <Text fontSize="sm">{project.description}</Text>
+        </GridItem>
       </SimpleGrid>
     );
   });
 };
 
-export default function Portifolio() {
-  return <Flex justify="center">{myProjects()}</Flex>;
-}
+export default myProjects;
+// export default function Portifolio() {
+//   return <Flex justify="center">{myProjects()}</Flex>;
+// }
 
 // server side rendering the image
 // const projectImages = () => {
