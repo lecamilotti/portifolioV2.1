c0991848152169f950f9372b969374f32000f241
Leandro Camilotti <lecamilotti@gmail.com>
2022-01-11 21:57:11 +0100

    projects page

--- src/components/Portifolio/index.tsx
+++ src/components/Portifolio/index.tsx
@@ -140,9 +140,163 @@ export default function Portifolio() {
     </Grid>
   );
  } else {
-  return (
-    <Grid justify="center">
-      <Button
+  for (let i = 0; i < myProjects().length; i++) {
+   return (
+      <Grid justify="center">
+        {i === 0 ? (
+          <GridItem
+            w="80%"
+            minChildWidth="350px"
+            spacing="2rem"
+            m="auto"
+            mt="2rem" 
+            bg="gray.700" 
+            borderRadius="xl"
+            boxShadow="xl"
+          >
+            <Box> 
+              <Box
+                as="a"  
+                href={projects[i].link}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Image
+                  src={projects[i].image}
+                  alt={projects[i].alt}
+                  maxWidth="100%"
+                  maxHeight="100%"
+                  objectFit="cover"
+                  _hover={{ filter: "brightness(0.2)" }}
+                  transition="all 0.3s ease-in-out"
+                />
+              </Box>
+            </Box>
+            <GridItem
+              m="auto"
+              p="1rem"
+              alignItems="center"
+              mb="1rem"
+              align="center"
+            >
+              {projects[i].name}
+              <Flex justifyContent="center" mt="1rem" w="100%">
+                <HStack spacing="1rem" fontSize="lg">
+                  <Button
+                    as="a"
+                    href={projects[i].link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    variant="outline"
+                    _hover={{ filter: "brightness(0.5)" }}
+                    transition="all 0.3s ease-in-out"
+                    color="gray.50"
+                    leftIcon={<FaGlobe />}
+                  >
+                    <Text fontSize="sm">Live</Text>
+                  </Button>
+                  <Button
+                    as="a"
+                    href={projects[i].gitHub}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    variant="outline"
+                    _hover={{ filter: "brightness(0.5)" }}
+                    transition="all 0.3s ease-in-out"
+                    color="gray.50"
+                    leftIcon={<FaGithub />}
+                  >
+                    <Text fontSize="sm">GitHub</Text>
+                  </Button>
+                </HStack>
+              </Flex>
+              <Text fontSize="sm" mt="2rem">
+                {projects[i].description}
+              </Text>
+            </GridItem>
+          </GridItem>
+        ) : null}
+        {i === 1 ? (
+          <GridItem
+            w="80%"
+            minChildWidth="350px"
+            spacing="2rem"
+            m="auto"
+            mt="2rem"
+            bg="gray.700"
+            borderRadius="xl"
+            boxShadow="xl"
+          >
+            <Box>
+              <Box
+                as="a"
+                href={projects[i].link}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Image
+                  src={projects[i].image}     
+                  alt={projects[i].alt}
+                  maxWidth="100%"
+                  maxHeight="100%"
+                  objectFit="cover"
+                  _hover={{ filter: "brightness(0.2)" }}
+                  transition="all 0.3s ease-in-out"
+                />
+              </Box>
+            </Box>
+            <GridItem
+              m="auto"
+              p="1rem"
+              alignItems="center"
+              mb="1rem"
+              align="center"
+            >
+              {projects[i].name}
+              <Flex justifyContent="center" mt="1rem" w="100%">
+                <HStack spacing="1rem" fontSize="lg">
+                  <Button
+                    as="a"
+                    href={projects[i].link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    variant="outline"
+                    _hover={{ filter: "brightness(0.5)" }}
+                    transition="all 0.3s ease-in-out"
+                    color="gray.50"
+                    leftIcon={<FaGlobe />}
+                  >
+                    <Text fontSize="sm">Live</Text>
+                  </Button>
+                  <Button
+                    as="a"
+                    href={projects[i].gitHub}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    variant="outline"
+                    _hover={{ filter: "brightness(0.5)" }}
+                    transition="all 0.3s ease-in-out"
+                    color="gray.50"
+                    leftIcon={<FaGithub />}
+                  >
+                    <Text fontSize="sm">GitHub</Text>
+                  </Button>
+                </HStack>
+              </Flex>
+              <Text fontSize="sm" mt="2rem">
+                {projects[i].description}
+              </Text>
+            </GridItem>
+          </GridItem>
+        ) : null}
+        
+        
+      </Grid>
+      
+   )
+   
+  }
+  <Button
         as="a"
         w="20%"
         bg="gray.700"
@@ -159,9 +313,8 @@ export default function Portifolio() {
       >
         {showProjects ? "Hide Projects" : "Show Projects"}
       </Button>
-    </Grid>
-  )
-}
+ }
+
 
 // function setShowProjects(arg0: boolean): void {
 //   throw new Error("Function not implemented.");
